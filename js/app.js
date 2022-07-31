540a9c4f79561796e71140c39399d272905a7baa
Matias Herrera <matiasherrera57@gmail.com>
2022-07-31 19:01:53 -0300

    Matias-Js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
+// ARRAYS-MÉTODOS
+
+ let productos = ["Arco Iris",
+                  "Llaveros",
+                    "Espejos",
+                     "Animales"] ;
+ 
+                     alert(productos); 
+ let resultado = productos.push ("Lámparas");   
+                     alert(productos);               
+ 
+
+
 //CICLOS
 for (let i = 001; i <= 005; i++) {
     let nombreIngresado = prompt("Ingresar nombre completo:");
-    alert("Entrada  N° "+i+" Nombre: "+nombreIngresado);
+    alert("Usuario  N° "+i+" Nombre: "+nombreIngresado);
 }
 //FUNCIONES - VARIABLES
 
@@ -13,20 +26,25 @@ for (let i = 001; i <= 005; i++) {
      {  let numero1 = 1150;
         let numero2 = 170;
         let resultado = sumar(numero1, numero2);
-         alert("Valor entrada(Incluye IVA) $" + (resultado));
+         alert("Valor producto (Incluye IVA) $" + (resultado));
      }
     
 //CONDICIONALES - VARIABLES
    { var nombre, edad;
     nombre = prompt ('Por favor introduce tu nombre completo:');
     if (nombre =="") { alert ('No has introducido ningún nombre.'); }
-    else { alert ('Hola '+nombre + '. Bienvenido a nuestra Discoteca Nocturna, que te diviertas.'); }
+    else { alert ('Hola '+nombre + '. Bienvenido/a a nuestra Tienda, que te diviertas.'); }
     edad = prompt ('¿Que edad tienes?');
     edad = Number(edad);
-    if (edad >=1 && edad < 18) {alert ('Eres menor de edad, deberás ser acompañado por un adulto.');}
-    else if (edad >=18 && edad <=100) {alert ('Tu lugar ah sido reservado con éxito.');}
+    if (edad >=1 && edad < 18) {alert ('Eres menor de edad, deberás ser supervisado/a por un adulto.');}
+    else if (edad >=18 && edad <=100) {alert ('Fuiste registrado/a con éxito.');}
     else {alert ('No has introducido un valor válido de edad. ( '+edad+')');}
     }
+    
+
+    
+
+    
 
 
 
